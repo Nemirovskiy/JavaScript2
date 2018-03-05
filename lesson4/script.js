2611c53a0f4aafa55b22cd0e0d2a76393c4444f8
Николай <it-nikola@mail.ru>
2018-03-05 20:29:20 +0300

    добавлено решение 1 задания 4 урока

---
+++ lesson4/script.js
@@ -0,0 +1,45 @@
+// установки
+var control = '#control';   // контролируемый список вкладок
+var text = '#text';         // список с описанием
+var number = 3;             // активная вкадка при загрузке страницы
+
+$(document).ready(function () {
+    // если номер активной вкладки больше чем вкладок - установим 1
+    number = (number >= $(control).children().length) ? 1 : number-1;
+    // находим указанную вкладку и устанавливаем активность
+    $($(control).children()[number]).addClass('active');
+    $($(text).children()[number]).addClass('active');
+
+    // вешаем на событие нажатия - функцию
+    $(control).on('click','li',function () {
+        // сохраним нажатый элемент в переменную
+        var element = this;
+        // установим счетчик в 0
+        var i =0;
+        // сохраним список детей
+        var arr = $(text).children();
+        // запустим обработку детей контролируемого блока
+        $(control).children().each(
+            function () {
+                // если нет описания для этой вкадки - скопируем название вкаладки и установим как текст
+                if(arr[i] === undefined){
+                    $(this).clone().appendTo(text);
+                    // пересчитаем детей
+                    arr = $(text).children();
+                }
+                // если нажатый элемент = перебираемому, то установим класс активности
+                // нажатой вкадке и такому же по счету элементу из описания
+                if(this === element) {
+                    $(this).addClass('active');
+                    $(arr[i]).addClass('active');
+                }
+                // иначе - снимем у остальных класс активности
+                else {
+                    $(this).removeClass('active');
+                    $(arr[i]).removeClass('active');
+                }
+                i++; // увеличим счетчик
+            }
+        )
+    })
+})
